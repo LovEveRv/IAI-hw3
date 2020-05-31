4026810293a9b6508f17be0b29de0d5d1349c788
LovEver_Rv <yongweili.cn@gmail.com>
2020-05-31 16:19:36 +0800

    length is now optional

--- dataset.py
+++ dataset.py
@@ -5,17 +5,21 @@ import json
 
 
 def pad_to_len(tensor, length):
+    if length == -1:
+        return tensor  # no processing
+    
     cur_len = tensor.shape[0]
     if cur_len > length:
-        return tensor[:length, :]
+        return tensor[:length, :]  # cut down
+    
     embed_dim = tensor.shape[1]
-    zeros = torch.zeros((length - cur_len, embed_dim))
+    zeros = torch.zeros((length - cur_len, embed_dim))  # pad
     return torch.cat((tensor, zeros), dim=0)
 
 
 class SinaDataset(Dataset):
     
-    def __init__(self, src_path, length):
+    def __init__(self, src_path, length=-1):
         self.length = length
         with open(src_path, 'r', encoding='utf-8') as f:
             self.data = json.load(f)
